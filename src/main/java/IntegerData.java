0f0e6c1c25a5c117ffd82875687bf3eefadf7d78
plg-irk <nk-irk@mail.ru>
2021-09-04 20:06:36 +0800

    Initial commit

---
+++ src/main/java/IntegerData.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class IntegerData {
+
+    ArrayList<String> fileArrayList;
+    boolean sortType;
+
+    public IntegerData(ArrayList<String> fileArrayList, boolean sortType) {
+        this.fileArrayList = fileArrayList;
+        this.sortType = sortType;
+    }
+
+    public void readWriteWithSort() {
+
+        ArrayList<Integer> arrayListToFile = new ArrayList<>();
+        for (int i = 1; i < fileArrayList.size(); i++) {
+            try {
+                BufferedReader reader =
+                        new BufferedReader(new FileReader(fileArrayList.get(i)));
+                String strLine;
+                Integer intData;
+                while ((strLine = reader.readLine()) != null) {
+                    try {
+                        intData = Integer.parseInt(strLine);
+                        if (arrayListToFile.size() != 0) {
+                            for (int j = 0; j < arrayListToFile.size(); j++) {
+                                if (sortType && intData - arrayListToFile.get(j) <= 0) {
+                                    arrayListToFile.add(j, intData);
+                                    break;
+                                } else if (!sortType && intData - arrayListToFile.get(j) >= 0) {
+                                    arrayListToFile.add(j, intData);
+                                    break;
+                                } else if (j == (arrayListToFile.size() - 1)) {
+                                    arrayListToFile.add(intData);
+                                    break;
+                                }
+                            }
+                        } else arrayListToFile.add(intData);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Ошибка данных в файле: " + fileArrayList.get(i));
+                        System.out.println("Возможна потеря данных.");
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Не удается найти указанный файл: " + fileArrayList.get(i));
+                System.out.println("Часть данных может быть утерянна.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (PrintWriter writer =
+                     new PrintWriter(new FileWriter(fileArrayList.get(0)))) {
+            for (Integer s : arrayListToFile) {
+                writer.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean sorting = true;
+        String typeData = null;
+        ArrayList<String> fileArrayList = new ArrayList<>();
+        for (String str : args) {
+            if (str.equals("-a")) sorting = true;
+            else if (str.equals("-d")) sorting = false;
+            else if ("-s".equals(str)) typeData = "s";
+            else if ("-i".equals(str)) typeData = "i";
+            else fileArrayList.add(str);
+        }
+
+        if (fileArrayList.size() == 0)
+            System.out.println("Отсутствует имя выходного файла.");
+        else if (fileArrayList.size() <= 1)
+            System.out.println("Необходимо не менее одного входного файла.");
+        else {
+
+            if (typeData == null)
+                System.out.println("Тип данных неопределен");
+
+            else if (typeData.equals("s")) {
+                StringData stringData = new StringData(fileArrayList, sorting);
+                stringData.readWriteWithSort();
+
+            } else if (typeData.equals("i")) {
+                IntegerData integerData = new IntegerData(fileArrayList, sorting);
+                integerData.readWriteWithSort();
+            }
+        }
+    }
+}
+
